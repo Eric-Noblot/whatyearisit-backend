eeff267764c0ed47695a9e378b7a140c278f7e55
Eric-Noblot <eric.noblot@gmail.com>
2024-01-26 12:54:47 +0100

    add json

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/year', function(req, res, next) {
-  console.log('test')
   const date = new Date().getUTCFullYear()
   res.json(({year : date}))
 });
